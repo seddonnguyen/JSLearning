107005c9e4c2a48d9a5c7de4cbe046483e8f8ed8
seddonnguyen <tri.seddon.nguyen@gmail.com>
2023-12-27 20:45:15 +0700

    Adjacency List - Course Schedule

---
+++ CourseSchedule.js
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/course-schedule/
+
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+const canFinish = (numCourses, prerequisites) => {
+    let preMap = new Map();
+    let visitSet = new Set();
+
+    for(let i = 0; i < numCourses; i++) {
+        preMap.set(i, []);
+    }
+
+    for(let c of prerequisites) {
+        let course = c[0];
+        preMap.set(course, [...preMap.get(course), c[1]]);
+    }
+
+    const dfs = course => {
+        if(visitSet.has(course)) {
+            return false;
+        }
+
+        let preReq = preMap.get(course);
+
+        if(preReq.length === 0) {
+            return true;
+        }
+
+        visitSet.add(course);
+
+        for(let c of preReq) {
+            if(!dfs(c)) {
+                return false;
+            }
+        }
+
+        visitSet.delete(course);
+        preMap.set(course, []);
+        return true;
+    };
+
+    for(let i = 0; i < numCourses; i++) {
+        if(!dfs(i)) {
+            return false;
+        }
+    }
+    return true;
+};
