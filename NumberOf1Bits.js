9be868be7880a16219f4c5f252508fa7347d76f3
seddonnguyen <tri.seddon.nguyen@gmail.com>
2023-12-28 12:42:16 +0700

    Count numbers of 1 bits

---
+++ NumberOf1Bits.js
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/number-of-1-bits/
+// Write a function that takes the binary representation of an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+// Example 1:
+// Input: n = 00000000000000000000000000001011
+// Output: 3
+// Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+//
+// Example 2:
+// Input: n = 00000000000000000000000010000000
+// Output: 1
+// Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+//
+// Example 3:
+// Input: n = 11111111111111111111111111111101
+// Output: 31
+// Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+const hammingWeight = function(n) {
+    let res = 0;
+    while(n !== 0) {
+        n = n & (n - 1);
+        res++;
+    }
+    return res;
+};
