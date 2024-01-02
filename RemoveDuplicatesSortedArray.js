9c6395150d808fd25051739bf7a2083a6f7f17dd
seddonnguyen <tri.seddon.nguyen@gmail.com>
2024-01-02 11:43:58 +0700

    Optimize code with for loop

--- RemoveDuplicatesSortedArray.js
+++ RemoveDuplicatesSortedArray.js
@@ -36,14 +36,12 @@
  */
 const removeDuplicates = function(nums) {
     let left = 1;
-    let right = 1;
 
-    while(right < nums.length) {
+    for(let right = 1; right < nums.length; right++) {
         if(nums[right] !== nums[right - 1]) {
             nums[left] = nums[right];
             left++;
         }
-        right++;
     }
     return left;
 };
