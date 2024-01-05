43ae498f1d20389c7370b1be3ddc8d10b5ae3be7
seddonnguyen <tri.seddon.nguyen@gmail.com>
2024-01-05 17:43:31 +0700

    fix naming issues, and add update and rangeQuery functions

--- SegmentTree.js
+++ SegmentTree.js
@@ -1,22 +1,53 @@
 class SegmentTree {
-    constructor(sum, left, right) {
+    constructor(sum, leftIndex, rightIndex) {
         this.sum = sum;
-        this.left = left;
-        this.right = right;
+        this.leftIndex = leftIndex;
+        this.rightIndex = rightIndex;
         this.leftTree = null;
         this.rightTree = null;
     }
 
-    static build(numbers, left, right) {
-        if(left === right) {
-            return new SegmentTree(numbers[left], left, right);
+    static build(numbers, leftIndex, rightIndex) {
+        if(leftIndex === rightIndex) {
+            return new SegmentTree(numbers[leftIndex], leftIndex, rightIndex);
         }
-        const mid = Math.floor((left + right) / 2);
-        const root = new SegmentTree(0, left, right);
-        root.leftTree = SegmentTree.build(numbers, left, mid);
-        root.rightTree = SegmentTree.build(numbers, mid + 1, right);
-        root.sum = root.left.sum + root.right.sum;
+
+        const middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+        const root = new SegmentTree(0, leftIndex, rightIndex);
+        root.leftTree = SegmentTree.build(numbers, leftIndex, middleIndex);
+        root.rightTree = SegmentTree.build(numbers, middleIndex + 1, rightIndex);
+        root.sum = root.leftTree.sum + root.rightTree.sum;
         return root;
+    }
+
+    update(index, value) {
+        if(this.leftIndex === this.rightIndex) {
+            this.sum = value;
+            return;
+        }
+
+        const middleIndex = Math.floor((this.leftIndex + this.rightIndex) / 2);
+        if(index > middleIndex) {
+            this.rightTree.update(index, value);
+        } else {
+            this.leftTree.update(index, value);
+        }
+        this.sum = this.leftTree.sum + this.rightTree.sum;
+    }
+
+    rangeQuery(leftIndex, rightIndex) {
+        if(this.leftIndex === leftIndex && this.rightIndex === rightIndex) {
+            return this.sum;
+        }
+
+        const middleIndex = Math.floor((this.leftIndex + this.rightIndex) / 2);
+        if(leftIndex > middleIndex) {
+            return this.rightTree.rangeQuery(leftIndex, rightIndex);
+        } else if(rightIndex <= middleIndex) {
+            return this.leftTree.rangeQuery(leftIndex, rightIndex);
+        } else {
+            return this.leftTree.rangeQuery(leftIndex, middleIndex) + this.rightTree.rangeQuery(middleIndex + 1, rightIndex);
+        }
     }
 }
 
