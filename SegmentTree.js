998b2da9a7294c7f6c38117f3b0a529ab841abb0
seddonnguyen <tri.seddon.nguyen@gmail.com>
2024-01-05 17:16:53 +0700

    fix reference bugs

--- SegmentTree.js
+++ SegmentTree.js
@@ -3,8 +3,8 @@ class SegmentTree {
         this.sum = sum;
         this.left = left;
         this.right = right;
-        this.leftNode = null;
-        this.rightNode = null;
+        this.leftTree = null;
+        this.rightTree = null;
     }
 
     static build(numbers, left, right) {
@@ -13,8 +13,8 @@ class SegmentTree {
         }
         const mid = Math.floor((left + right) / 2);
         const root = new SegmentTree(0, left, right);
-        root.left = SegmentTree.build(numbers, left, mid);
-        root.right = SegmentTree.build(numbers, mid + 1, right);
+        root.leftTree = SegmentTree.build(numbers, left, mid);
+        root.rightTree = SegmentTree.build(numbers, mid + 1, right);
         root.sum = root.left.sum + root.right.sum;
         return root;
     }
