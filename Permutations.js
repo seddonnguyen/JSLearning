ea1a327e7d7059e35688b0c3a4175194d03ba1a9
seddonnguyen <tri.seddon.nguyen@gmail.com>
2024-01-07 18:38:49 +0700

    LeetCode 46. Permutations

--- Permutations.js
+++ Permutations.js
@@ -1,25 +1,44 @@
+//https://leetcode.com/problems/permutations/
+// LeetCode 46. Permutations
+// Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+//
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+//
+// Example 2:
+// Input: nums = [0,1]
+// Output: [[0,1],[1,0]]
+//
+// Example 3:
+// Input: nums = [1]
+// Output: [[1]]
+//
+// Constraints:
+// 1 <= nums.length <= 6
+// -10 <= nums[i] <= 10
+// All the integers of nums are unique.
+
 /**
  * @return {number[][]}
  * @param word
  */
-
-//https://leetcode.com/problems/permutations/description/
 const permute = word => {
-    let result = [];
-    let letters = word.split("");
+    const result = [];
+    const letters = word.split("");
+    const length = letters.length;
 
-    if(letters.length === 1) {
-        return [[...letters]];
+    if(length === 1) {
+        return [[letters[0]]];
     }
-    let length = letters.length;
     for(let i = 0; i < length; i++) {
-        let n = letters.shift();
-        let perms = permute(letters.join(""));
-        for(let p of perms) {
-            p.push(n);
+        let l = letters.shift();
+        let permutations = permute(letters.join(""));
+        for(let p of permutations) {
+            p.push(l);
         }
-        result.push(...perms);
-        letters.push(n);
+        result.push(...permutations);
+        letters.push(l);
     }
     return result;
 };
